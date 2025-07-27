270542f0076130e05fc31fc92c431d2616b64df3
Ravindu-Rasanjana <ravras17@gmail.com>
2025-07-27 23:38:58 +0530

    Finally working with basic functions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,10 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
